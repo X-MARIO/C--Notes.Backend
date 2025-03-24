b7f0eb4f74c04d7422551376e5bcbf5b8f5d44ec
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-03-24 22:47:45 +0400

    create not configuration

---
+++ Notes.Persistence/NotesDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Persistence.EntityTypeConfigurations;
+
+namespace Notes.Persistence;
+
+public class NotesDbContext(DbContextOptions<NotesDbContext> options) : DbContext(options), INotesDbContext
+{
+    public DbSet<Note> Notes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new NoteConfiguration());
+        base.OnModelCreating(builder);
+    }
+}
